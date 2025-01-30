4b5d009d3850f6c75e0bff13cce5aed64a0210a8
Allan <a@gmail.com>
2025-01-30 11:03:52 -0300

    allan

---
+++ src/main/java/com/senai/aula01_introducao_poo/exemplos/Pessoa.java
@@ -0,0 +1,12 @@
+package com.senai.aula01_introducao_poo.exemplos;
+
+public class Pessoa {
+    String nome;
+    int idade;
+    float altura;
+    String endereco;
+
+    public void falar(String frase){
+        System.out.println(frase);
+    }
+}
