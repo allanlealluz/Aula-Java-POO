3903d5e7d34035df39f5337739f98f823fed003f
Allan <a@gmail.com>
2025-02-06 11:05:10 -0300

    changes 06/02 1

--- src/main/java/com/senai/Main.java
+++ src/main/java/com/senai/Main.java
@@ -3,8 +3,11 @@ package com.senai;
 import com.senai.aula01_introducao_poo.exemplos.pessoa.Endereco;
 import com.senai.aula01_introducao_poo.exemplos.pessoa.Pessoa;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
         Pessoa pessoa1 = new Pessoa(
                 new Endereco("Grajau",
                              "grauna do baile",
@@ -25,7 +28,18 @@ public class Main {
                 12,
                 "Allan");
         String[] etiqueta = {"Nome","Idade","Altura","Endereço\n","Rua","Logradouro","Cidade","numeroCasa","bairro"};
-        System.out.println(etiqueta);
+        String[] dados = new String[etiqueta.length];
+        System.out.println("Preencha os dados da pessoa criada");
+        for (int i = 0; i < etiqueta.length; i++) {
+            System.out.println(etiqueta[i]);
+            dados[i] = scanner.nextLine();
+        }
+        Pessoa pessoa3  = new Pessoa(
+                new Endereco(
+                      dados[1],
+                ),
+                dados[0],
+        );
     }
 
 
