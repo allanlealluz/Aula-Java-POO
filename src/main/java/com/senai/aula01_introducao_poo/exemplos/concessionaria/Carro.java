111d938554e387639d5530d851dc3dcad2cc74a2
Allan <a@gmail.com>
2025-02-06 10:30:21 -0300

    changes 06/02 1

--- src/main/java/com/senai/aula01_introducao_poo/exemplos/concessionaria/Carro.java
+++ src/main/java/com/senai/aula01_introducao_poo/exemplos/concessionaria/Carro.java
@@ -7,16 +7,37 @@ public class Carro {
     String cor;
     float motor;
     float preco;
+    float kilometragem;
+    String NomeDono;
+    boolean Eletrico;
 
-    public Carro(int ano, String marca, String modelo,String cor, float motor, float preco) {
+    public Carro(int ano, String marca, String modelo, String cor, float motor, float preco, float kilometragem, String nomeDono, boolean eletrico) {
         this.ano = ano;
         this.marca = marca;
         this.modelo = modelo;
         this.cor = cor;
         this.motor = motor;
         this.preco = preco;
+        this.kilometragem = kilometragem;
+        NomeDono = nomeDono;
+        Eletrico = eletrico;
     }
-
+    public void testDrive(){
+        System.out.println("Veiculo "+modelo+ " foi testado e aprovado");
+    }
+    public void comprar(String nomeCliente){
+        System.out.println("Parabéns pela compra "+nomeCliente+ " do "+marca+" "+modelo);
+    }
+    public void valorFipe(int ano){
+        int idade = ano - this.ano;
+        double valorAtual = preco;
+        for (int i = 0; i < idade; i++) {
+            valorAtual = valorAtual - 0.02f*valorAtual;
+        }
+        float valorAtual2 = (float) ( preco * Math.pow(1- 0.02f,idade));
+        System.out.println("Valor atual 2: "+valorAtual2);
+        System.out.printf("O modelo %s do ano %d R$%,.2f", modelo,ano, valorAtual);
+    }
     @Override
     public String toString() {
         return "Carro{" +
@@ -26,6 +47,9 @@ public class Carro {
                 ", cor='" + cor + '\'' +
                 ", motor=" + motor +
                 ", preco=" + preco +
+                ", kilometragem=" + kilometragem +
+                ", NomeDono='" + NomeDono + '\'' +
+                ", Eletrico=" + Eletrico +
                 '}';
     }
 }
--- src/main/java/com/senai/aula01_introducao_poo/exemplos/concessionaria/Main.java
+++ src/main/java/com/senai/aula01_introducao_poo/exemplos/concessionaria/Main.java
@@ -2,7 +2,14 @@ package com.senai.aula01_introducao_poo.exemplos.concessionaria;
 
 public class Main {
     public static void main(String[] args) {
-        Carro carro1 = new Carro(2021,"Hyundai","HB20","azul",1.4f,200.00f);
+        Carro carro1 = new Carro(2021,"Hyundai","HB20","azul",1.4f,200.00f,150,"Jorge",false);
+        Carro carro2 = new Carro(1990, "Volkswagen","Fusca", "Verde",1.0f,15.00f,500,"Alloene",false);
         System.out.println(carro1);
+        System.out.println(carro2);
+        carro1.testDrive();
+        carro2.testDrive();
+        carro1.comprar("Allan");
+        carro2.comprar("Bruno");
+        carro1.valorFipe(2025);
     }
 }
