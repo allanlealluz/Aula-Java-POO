f2e7918055373bda57192ad22018dcbada632b43
Allan <a@gmail.com>
2025-02-07 10:43:54 -0300

    changes 07/02 4

---
+++ src/main/java/com/senai/aula01_introducao_poo/exercicios/TesteEx1/Main.java
@@ -0,0 +1,13 @@
+package com.senai.aula01_introducao_poo.exercicios.TesteEx1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] vetorProduto = new String[3];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("");
+        Produto produto = new Produto("cereal",12,15.2f);
+    }
+}
---
+++ src/main/java/com/senai/aula01_introducao_poo/exercicios/TesteEx1/Produto.java
@@ -0,0 +1,14 @@
+package com.senai.aula01_introducao_poo.exercicios.TesteEx1;
+
+public class Produto {
+    String nome;
+    int estoque;
+    float valor;
+
+    public Produto(String nome, int estoque, float valor) {
+        this.nome = nome;
+        this.estoque = estoque;
+        this.valor = valor;
+    }
+
+}
