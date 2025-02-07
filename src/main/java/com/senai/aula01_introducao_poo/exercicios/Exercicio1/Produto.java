87c0c58adc9a3af85e288005fe483c16bd743962
Allan <a@gmail.com>
2025-02-07 09:23:54 -0300

    changes 07/02 2

--- src/main/java/com/senai/aula01_introducao_poo/exercicios/Exercicio1/Produto.java
+++ src/main/java/com/senai/aula01_introducao_poo/exercicios/Exercicio1/Produto.java
@@ -1,9 +1,12 @@
 package com.senai.aula01_introducao_poo.exercicios.Exercicio1;
 
+import java.util.Scanner;
+
 public class Produto {
     String nome;
     float preco;
     int quantidade;
+    Scanner scanner = new Scanner(System.in);
 
     public Produto(String nome, float preco, int quantidade) {
         this.nome = nome;
@@ -15,7 +18,16 @@ public class Produto {
         System.out.println("Produto "+nome+" custa "+preco+" e possui "+quantidade+ " itens");
     }
     public void atualizaEstoque (int valor){
-        quantidade += valor;
+        System.out.println("Vai acrescentar ou reduzir (S/N) ");
+        String resp =  scanner.nextLine();
+        if(resp.toLowerCase().equals("s")){
+            quantidade += valor;
+        }else if(resp.toLowerCase().equals("n")){
+            quantidade -= valor;
+        }else{
+            System.out.println("Erro");
+        }
+
         System.out.println("Agora possui o produto "+nome+" possui "+quantidade+ " itens");
     }
     public void calcularValorEstoque(){
