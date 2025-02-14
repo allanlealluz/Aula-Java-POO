0bd6684332c5a19927ab766e565f72382e6825a1
Allan <allanlealluz@gmail.com>
2025-02-14 10:03:46 -0300

    commit 14/02 3

--- src/main/java/com/senai/aula02_colecoes/Main.java
+++ src/main/java/com/senai/aula02_colecoes/Main.java
@@ -1,12 +1,14 @@
 package com.senai.aula02_colecoes;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        Pessoa pessoa1 = new Pessoa(36,"Rafael");
-        Pessoa pessoa2 = new Pessoa(17,"Toupeira");
-        Pessoa pessoa3 = new Pessoa(19,"Gabriel");
+        Pessoa pessoa1 = new Pessoa("Rafael",40);
+        Pessoa pessoa2 = new Pessoa("Gislene",32);
+        Pessoa pessoa3 = new Pessoa("Lara",6);
+        //-------------usando matriz-------------------------------------
 
         Pessoa[] matrizPessoas = new Pessoa[4];
 
@@ -14,12 +16,15 @@ public class Main {
         matrizPessoas[1] = pessoa2;
         matrizPessoas[2] = pessoa3;
 
-        for(Pessoa pessoa : matrizPessoas){
+        for (Pessoa pessoa : matrizPessoas) {
             System.out.println(pessoa);
         }
-        matrizPessoas[0].falar("Dale");
-        matrizPessoas[3] = new Pessoa(43,"Helena");
+
+        matrizPessoas[0].falar("Oi!!");
+        matrizPessoas[3] = new Pessoa("Helena",65);
         System.out.println(matrizPessoas[3].nome);
+
+        //--------------usando ArrayList--------------------------------
 
         ArrayList lista = new ArrayList();
 
@@ -34,6 +39,27 @@ public class Main {
         ArrayList<Pessoa> listaPessoas = new ArrayList();
 
         listaPessoas.add(pessoa1);
-        listaPessoas.add(new Pessoa(19, "Gabriel"));
+        listaPessoas.add(new Pessoa("José", 20));
+
+        System.out.println("tamanho da lista: "+listaPessoas.size());
+        listar(listaPessoas);
+        listaPessoas.remove(pessoa1);
+        listar(listaPessoas);
+
+        listaPessoas.set(0,pessoa2);
+        listar(listaPessoas);
+
+        listaPessoas.add(0,pessoa3);
+        listar(listaPessoas);
+
+
+    }
+
+    public static void listar(ArrayList<Pessoa> lista){
+        System.out.println("\n--------lista--------");
+        for (Pessoa pessoa: lista){
+            System.out.println(pessoa);
+        }
+        System.out.println("---------------------");
     }
 }
--- src/main/java/com/senai/aula02_colecoes/Pessoa.java
+++ src/main/java/com/senai/aula02_colecoes/Pessoa.java
@@ -4,7 +4,7 @@ public class Pessoa {
     String nome;
     int idade;
 
-    public Pessoa(int idade, String nome) {
+    public Pessoa(String nome, int idade) {
         this.idade = idade;
         this.nome = nome;
     }
