a8a93ec24e22dc2c9eef27f8548c59b1cc8638ab
Aleph <maria.alexandrina.luz@gmail.com>
2025-02-21 10:38:39 -0300

    commitada 21/02 exercicio 1 aula 3

--- src/main/java/com/senai/aula03_encapsulamento/Main.java
+++ src/main/java/com/senai/aula03_encapsulamento/Main.java
@@ -1,4 +1,22 @@
 package com.senai.aula03_encapsulamento;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("SISTEMA INVENTÁRIO");
+        System.out.println("Digite o nome do produto que quer adicionar ");
+        String nomeProd = scanner.nextLine();
+        System.out.println("Qual o valor do produto ");
+        Double valor= scanner.nextDouble();
+        Produto arroz = new Produto(nomeProd,valor);
+        arroz.setNome("Milho");
+        System.out.println(arroz.getNome());
+
+
+
+    }
+
+
 }
---
+++ src/main/java/com/senai/aula03_encapsulamento/Produto.java
@@ -0,0 +1,33 @@
+package com.senai.aula03_encapsulamento;
+
+public class Produto {
+    private String nome;
+    private Double preco;
+
+    public Produto(String nome, Double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        if(preco > 0){
+            this.preco = preco;
+        }else{
+            throw new IllegalArgumentException("Produto com preço negativo!!");
+        }
+
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
