642c84a512237457517e2a2767845f883101b106
= <=>
2025-03-21 10:32:25 -0300

    commitada 2.0

--- src/main/java/com/senai/aula05_polimorfismo/Exemplos/Animal/Animal.java
+++ src/main/java/com/senai/aula05_polimorfismo/Exemplos/Animal/Animal.java
@@ -1,4 +1,10 @@
 package com.senai.aula05_polimorfismo.Exemplos.Animal;
 
 public class Animal {
+    public Animal() {
+    }
+
+    public void fazerSom(){
+        System.out.println("Dajidjsifjaijfdia");
+    }
 }
--- src/main/java/com/senai/aula05_polimorfismo/Exemplos/Animal/Cachorro.java
+++ src/main/java/com/senai/aula05_polimorfismo/Exemplos/Animal/Cachorro.java
@@ -1,4 +1,11 @@
 package com.senai.aula05_polimorfismo.Exemplos.Animal;
 
-public class Cachorro {
+public class Cachorro extends Animal{
+    public Cachorro() {
+    }
+
+    @Override
+    public void fazerSom(){
+        System.out.println("auauauauuaauau");
+    }
 }
