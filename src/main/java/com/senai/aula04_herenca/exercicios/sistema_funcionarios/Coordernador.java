599d98bdcf722273a6d60fc9b71f13f5b86d5fcc
Allan <allanlealluz@gmail.com>
2025-03-27 04:12:53 -0300

    COMMITADA 3

--- src/main/java/com/senai/aula04_herenca/exercicios/sistema_funcionarios/Coordernador.java
+++ src/main/java/com/senai/aula04_herenca/exercicios/sistema_funcionarios/Coordernador.java
@@ -1,18 +1,27 @@
 package com.senai.aula04_herenca.exercicios.sistema_funcionarios;
 
-public class Coordernador extends Funcionario{
-    private String equipeProfessores;
+import java.util.ArrayList;
 
-    public Coordernador(String nome, double salario, String equipeProfessores) {
+public class Coordernador extends Funcionario {
+    private ArrayList<Professor> equipeProfessores;
+
+    public Coordernador(String nome, double salario, ArrayList<Professor> equipeProfessores) {
         super(nome, salario);
         this.equipeProfessores = equipeProfessores;
     }
 
-    public String getEquipeProfessores() {
+    public ArrayList<Professor> getEquipeProfessores() {
         return equipeProfessores;
     }
 
-    public void setEquipeProfessores(String equipeProfessores) {
+    public void setEquipeProfessores(ArrayList<Professor> equipeProfessores) {
         this.equipeProfessores = equipeProfessores;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("O coordenador " + getNome() + " controla a equipe: ");
+        for (Professor professor : equipeProfessores) {
+            System.out.println(professor.getNome());
+        }
     }
 }
--- src/main/java/com/senai/aula04_herenca/exercicios/sistema_funcionarios/Main.java
+++ src/main/java/com/senai/aula04_herenca/exercicios/sistema_funcionarios/Main.java
@@ -1,8 +1,15 @@
 package com.senai.aula04_herenca.exercicios.sistema_funcionarios;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Professor professor1 = new Professor("Valdo",2500,"Filosofia");
-        Coordernador coordernador1 = new Coordernador("Rodrigo", 4500, "Jorginos");
+        ArrayList<Professor> professores = new ArrayList<>();
+        professores.add(new Professor ("Valdo",2500,"Filosofia"));
+        professores.add(new Professor ("Cida",3900,"Português"));
+        professores.add(new Professor ("João Carlos",3500,"Matemática"));
+        Coordernador coordenador = new Coordernador("Tonhão", 5800, professores);
+        coordenador.exibirInformacoes();
+
     }
 }
